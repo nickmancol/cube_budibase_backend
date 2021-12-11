5bde66c78f1d43cbbfa7126e6af0d9059deb83f8
nicolas.bohorquez <nicolas.bohorquez@alephsa.com>
2021-12-11 20:58:45 +0000

    Updated orders schema

--- schema/Orders.js
+++ schema/Orders.js
@@ -1,14 +1,5 @@
 cube(`Orders`, {
   sql: `SELECT * FROM public.orders`,
-  
-  preAggregations: {
-    // Pre-Aggregations definitions go here
-    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
-  },
-  
-  joins: {
-    
-  },
 
   segments: {
     Consumer: {
